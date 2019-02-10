4a87797582b772675e38b5513682ada64bde042d
phs1234 <32615067+phs1234@users.noreply.github.com>
2019-02-10 23:02:32 +0900

    #2 Add simple tab

--- app/src/main/java/com/example/hellgarammobileapp/main/MainActivity.java
+++ app/src/main/java/com/example/hellgarammobileapp/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hellgarammobileapp.main;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,18 +12,45 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private ContentsPagerAdapter contentsPagerAdapter;
+    private TabLayout tabLayout;
+
 
 //    Typeface typeface = Typeface.createFromAsset(this.getResources().getAssets(), "fonts/DXRMbxB-KSCpc-EUC-H.mp3");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tabLayout = findViewById(R.id.bottomtablayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("급식"));
+        tabLayout.addTab(tabLayout.newTab().setText("시간표"));
+        tabLayout.addTab(tabLayout.newTab().setText("버스"));
+
         viewPager = findViewById(R.id.viewpager);
         contentsPagerAdapter = new ContentsPagerAdapter(getSupportFragmentManager());
 
         viewPager.setAdapter(contentsPagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
 //    @Override
