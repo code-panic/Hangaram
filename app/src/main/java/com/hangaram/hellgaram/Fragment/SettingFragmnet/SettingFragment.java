755bb1764ce1a8e7ab0fcc00d99d457deac18db4
phs1234 <32615067+phs1234@users.noreply.github.com>
2019-03-04 01:03:51 +0900

    #27 Recover Quote func

--- app/src/main/java/com/hangaram/hellgaram/Fragment/SettingFragmnet/SettingFragment.java
+++ app/src/main/java/com/hangaram/hellgaram/Fragment/SettingFragmnet/SettingFragment.java
@@ -2,36 +2,58 @@ package com.hangaram.hellgaram.Fragment.SettingFragmnet;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.hangaram.hellgaram.R;
 import com.hangaram.hellgaram.main.CreditActivity;
+import com.hangaram.hellgaram.support.QuoteGiver;
+import com.hangaram.hellgaram.support.TimeGiver;
 
-public class SettingFragment extends Fragment{
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class SettingFragment extends Fragment {
+    private static String log = "SettingFragment";
+
     private View view;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter themeItemAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private ImageView buttonTocredit;
 
+    public TextView quote;
+    public TextView hint;
+
     public static final int REQUEST_CODE_CREDIT = 101;
 
+    private QuoteGiver quoteGiver;
+
     @Override
-    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.activity_theme,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_theme, container, false);
         init(view.getContext());
         return view;
     }
 
-    private void init(Context context){
+    private void init(Context context) {
         recyclerView = view.findViewById(R.id.themeRecyclerView);
         buttonTocredit = view.findViewById(R.id.buttonToCredit);
 
@@ -39,19 +61,25 @@ public class SettingFragment extends Fragment{
         recyclerView.setLayoutManager(layoutManager);
 
         themeItemAdapter = new ThemeItemAdapter();
-        ((ThemeItemAdapter) themeItemAdapter).add(new ThemeItem("기본",R.drawable.white_bg));
+        ((ThemeItemAdapter) themeItemAdapter).add(new ThemeItem("기본", R.drawable.white_bg));
 
         recyclerView.setAdapter(themeItemAdapter);
 
         buttonTocredit.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     Intent intent = new Intent(getContext(), CreditActivity.class);
                     startActivityForResult(intent, REQUEST_CODE_CREDIT);
                 }
                 return true;
             }
         });
+
+        quote = view.findViewById(R.id.quote);
+        hint = view.findViewById(R.id.hint);
+
+        quoteGiver = new QuoteGiver(context,this);
     }
+
 }
---
+++ app/src/main/java/com/hangaram/hellgaram/support/QuoteGiver.java
@@ -0,0 +1,103 @@
+package com.hangaram.hellgaram.support;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.hangaram.hellgaram.Fragment.SettingFragmnet.SettingFragment;
+import com.hangaram.hellgaram.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class QuoteGiver {
+    private static final String PACKAGE_DIR = "/data/data/com.hangaram.hellgaram/";
+    private static final String DATABASE_NAME = "UpdateDatabase.db";
+    private static final String TABLE_NAME = "quotes";
+
+    private static int VERSION_CODE = 3;
+    SharedPreferences sharedPreferences;
+
+    private static String log = "QuoteGiver";
+
+    private String quote;
+    private String hint;
+
+    public QuoteGiver(Context context, SettingFragment settingFragment) {
+        sharedPreferences = context.getSharedPreferences("setting", MODE_PRIVATE);
+        initialize(context);
+
+        try {
+            SQLiteDatabase db = context.openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+
+            String[] args = {TimeGiver.getMonth(), TimeGiver.getDate()};
+
+            Cursor cursor = db.rawQuery("SELECT month,date,quote,hint From " + TABLE_NAME +
+                    " where month = ? AND date = ?", args);
+
+            Log.d(log, "cursor Count: " + cursor.getCount());
+
+            cursor.moveToFirst();
+
+            Log.d(log,"quote: " + cursor.getString(2));
+            Log.d(log,"hint: " + cursor.getString(3));
+
+            settingFragment.quote.setText(cursor.getString(2));
+            settingFragment.hint.setText(cursor.getString(3));
+
+            cursor.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void initialize(Context context) {
+        File folder = new File(PACKAGE_DIR + "databases");
+
+        if (!folder.exists())
+            folder.mkdirs();
+
+        File outfile = new File(PACKAGE_DIR + "databases/" + DATABASE_NAME);
+
+        Log.d(log, "current VersionCode = " + sharedPreferences.getInt("versionCode", 1));
+        Log.d(log, "VERSION_CODE = " + VERSION_CODE);
+
+        if (outfile.length() <= 0 || sharedPreferences.getInt("versionCode", 1) != VERSION_CODE) {
+            AssetManager assetManager = context.getResources().getAssets();
+            try {
+                InputStream inputStream = assetManager.open(DATABASE_NAME, AssetManager.ACCESS_BUFFER);
+                long filesize = inputStream.available();
+                byte[] tempdata = new byte[(int) filesize];
+
+                inputStream.read(tempdata);
+                inputStream.close();
+
+                outfile.createNewFile();
+
+                FileOutputStream fileOutputStream = new FileOutputStream(outfile);
+                fileOutputStream.write(tempdata);
+                fileOutputStream.close();
+
+                SharedPreferences.Editor ed = sharedPreferences.edit();
+                ed.putInt("versionCode", VERSION_CODE);
+                ed.commit();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        File list[] = folder.listFiles();
+
+        for(File file: list){
+            Log.d(log,"file list: " + file.getName() + "\n");
+        }
+
+    }
+}
